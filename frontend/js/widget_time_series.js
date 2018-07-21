84c40d7d9f11bb9829186c2a274c403462f02512
John Williamson <johnhw@gmail.com>
2018-07-21 09:08:08 +0100

    TImeline labels implemented

--- frontend/js/widget_time_series.js
+++ frontend/js/widget_time_series.js
@@ -1,5 +1,6 @@
-var _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
+var MS_PER_HOUR = 1000 * 60 * 60;
+var MS_PER_DAY = MS_PER_HOUR * 24;
 
 // end
 
@@ -19,7 +20,8 @@ function time_xpos(date)
     var now = new Date(Date.now());
     var today_date = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     // period through the day, as a value 0.0 -> 1.0
-    var interpolate = (now-today_date) / _MS_PER_DAY;    
+    var interpolate = (date-today_date) / MS_PER_DAY; 
+    console.log(date, today_date)   ;
     // today
     if(interpolate<1.0)
     {        
@@ -34,6 +36,36 @@ function time_xpos(date)
 
 }
 
+function day_labels(date, bbox)
+{
+    var g = draw.group();
+    [0,3,6,9,12,15,18,21,24,30,36,42].forEach(function(hour_offset)
+    {
+        var hour_date = new Date(date.getTime() + hour_offset * MS_PER_HOUR);
+        
+        var xpos = time_xpos(hour_date); // centre point of label
+        var ypos = bbox.cy;
+        
+        var text_label = draw.text(pad(hour_offset % 24,2)+"00").id("text_style");
+        var font_size;
+        // Set labels so that intermediate hours (e.g. 0300) appear smaller
+        if(((hour_offset/3)%2)===1)
+        {
+            font_size = 14;
+        }
+        else
+        {
+            font_size = 24;
+        }
+        text_label.font({"size":font_size});
+        var tbbox = text_label.bbox();
+        // centre the text
+        text_label.move(xpos-tbbox.w/2, ypos-tbbox.h/2);        
+        g.add(text_label);
+    });
+    return g;
+}
+
 // Simple time display
 widget_time_labels = {
     init:function(bbox){                
@@ -43,11 +75,9 @@ widget_time_labels = {
         // compute the date objects for 00:00 today and tomorrow
         var today_date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
         var tomorrow = new Date(today_date.valueOf());
-        tomorrow.setDate(tomorrow.getDate()+1);        
-        // find the position on the x scale
-        var xpos = time_xpos(today_date);
-        var ypos = bbox.cy;
-        draw.text("NOW").move(xpos, ypos).id("text_style");        
+        tomorrow.setDate(tomorrow.getDate()+1);                
+        day_labels(today_date, bbox);
+     
     },
 
     
